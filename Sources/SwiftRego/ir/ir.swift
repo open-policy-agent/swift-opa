09ea650f53f6fd830c8b2ba052c10c25269bfd72
Oren Shomron <shomron@apple.com>
2024-11-26 12:33:32 -0800

    adding operand tests
    Signed-off-by: Oren Shomron <shomron@apple.com>

--- Sources/SwiftRego/ir/ir.swift
+++ Sources/SwiftRego/ir/ir.swift
@@ -1,35 +1,34 @@
-struct Policy : Codable {
-    var staticData : Static
-    var plans : Plans = Plans()
-    var funcs : Funcs = Funcs()
-    
-    enum CodingKeys : String, CodingKey {
+struct Policy: Codable, Equatable {
+    var staticData: Static?
+    var plans: Plans? = nil
+    var funcs: Funcs? = nil
+
+    enum CodingKeys: String, CodingKey {
         case staticData = "static"
         case plans
         case funcs
     }
 }
 
-struct Static : Codable {
-    var strings : [ConstString]
-    var builtinFuncs : [BuiltinFunc]?
-    var files : [ConstString]
-    
-    enum CodingKeys : String, CodingKey {
+struct Static: Codable, Equatable {
+    var strings: [ConstString]
+    var builtinFuncs: [BuiltinFunc]?
+    var files: [ConstString]
+
+    enum CodingKeys: String, CodingKey {
         case strings
         case builtinFuncs = "builtin_funcs"
         case files
     }
 }
 
-struct ConstString : Codable {
-    var value : String
+struct ConstString: Codable, Equatable {
+    var value: String
 }
 
-
-struct BuiltinFunc : Codable {
-    var name : String
-//    var decl : FunctionDecl
+struct BuiltinFunc: Codable, Equatable {
+    var name: String
+    //    var decl : FunctionDecl
 }
 
 /*
@@ -38,62 +37,152 @@ struct BuiltinFunc : Codable {
  var result Type
  var variadic Type
  }
- 
+
  struct Type : Codable {
- 
+
  }
  */
 
-struct Plans : Codable {
+struct Plans: Codable, Equatable {
     var plans: [Plan] = []
 }
 
-struct Plan : Codable {
-    var name : String
-    var blocks : [Block]
+struct Plan: Codable, Equatable {
+    var name: String
+    var blocks: [Block]
 }
 
-struct Block : Codable {
+struct Block: Codable, Equatable {
     // TODO
-//    var stmts : [Statement]
+    enum CodingKeys: String, CodingKey {
+        case statements = "stmts"
+    }
+    // Each abstract statement has a stmt with the polymorphic contents
+    enum InnerCodingKeys: String, CodingKey {
+        case innerStatement = "stmt"
+    }
+    //var statements: Statements
+    var statements: [any Statement]
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var iter = try container.nestedUnkeyedContainer(forKey: .statements)
+        var peek = iter
+
+        var out = [any Statement]()
+        while !peek.isAtEnd {
+            let anyStmt = try peek.decode(AnyStatement.self)
+            let inner = try iter.nestedContainer(keyedBy: InnerCodingKeys.self)
+
+            var outStmt: any Statement
+
+            switch anyStmt.type {
+            case .CallStmt:
+                outStmt = try inner.decode(CallStatement.self, forKey: .innerStatement)
+            //            case .AssignVarStmt:
+            //            case .MakeObjectStmt:
+            //            case .ObjectInsertStmt:
+            //            case .ResultSetAddStmt:
+            default:
+                //                 TODO This wouldn't get here if the type was unknown :/
+                throw StatementError.Unknown(anyStmt.type.rawValue)
+            //                outStmt = CallStatement()
+            //                print("skipping \(anyStmt.type.rawValue)")
+            }
+
+            // Set location properties shared by any statement type
+            outStmt.location = anyStmt.location
+
+            out.append(outStmt)
+        }
+
+        self.statements = out
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        throw EncodingError.Unsupported
+    }
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        guard lhs.statements.count == rhs.statements.count else {
+            return false
+        }
+
+        // TODO
+
+        return true
+    }
+
 }
 
-struct Stmt : Codable {
-    enum type : String, Codable {
+// AnyStatement represents the generic parts of a statement - its type and location.
+// This is used for partial decoding of polymorphic statements preceding the concrete
+// statement decoding.
+struct AnyStatement: Codable, Equatable {
+    enum KnownStatements: String, Codable {
         case CallStmt
         case AssignVarStmt
         case MakeObjectStmt
         case ObjectInsertStmt
         case ResultSetAddStmt
     }
-}
-
-protocol Statement {
-    
-}
-
-struct CallStmt : Statement, Codable {
-    var callFunc : String
-    
-    enum CodingKeys : String, CodingKey {
-        case callFunc = "func"
-        // TODO args
+    enum CodingKeys: String, CodingKey {
+        case type
+        case location = "stmt"
     }
-    
+    var type: KnownStatements
+    var location: Location
 }
 
-struct Funcs : Codable {
-    var Funcs : [Func] = []
+struct Location: Codable, Equatable {
+    var row: Int = 0
+    var col: Int = 0
+    var file: Int = 0
 }
 
-struct Func : Codable {
-    var name : String
-    var path : [String]
-    var params : [Int]
-    var returnVar : Int
-    var blocks : [Block]
-    
-    enum CodingKeys : String, CodingKey {
+enum StatementError: Error {
+    case Unknown(String)
+}
+enum EncodingError: Error {
+    case Unsupported
+}
+
+// Statement is implemented by each conctete statement type
+protocol Statement: Codable, Equatable {
+    // Location coordinates, shared by all concrete statement
+    var location: Location { get set }
+}
+
+// -=-=-=-= CallStatement -=-=-=-=
+struct CallStatement: Statement, Codable, Equatable {
+    var location: Location = Location()
+
+    var callFunc: String = ""
+    var args: [Argument] = []
+
+    enum CodingKeys: String, CodingKey {
+        case callFunc = "func"
+        case args
+    }
+}
+
+struct Argument: Codable, Equatable {
+    var type: String
+    var value: Int
+}
+
+struct Funcs: Codable, Equatable {
+    var Funcs: [Func] = []
+}
+
+struct Func: Codable, Equatable {
+    var name: String
+    var path: [String]
+    var params: [Int]
+    var returnVar: Int
+    var blocks: [Block]
+
+    enum CodingKeys: String, CodingKey {
         case name
         case path
         case params
@@ -101,4 +190,60 @@ struct Func : Codable {
         case blocks
     }
 }
+// -=-=-=-= End CallStatement -=-=-=-=
 
+struct AssignVarStatement: Statement, Codable, Equatable {
+    var location: Location = Location()
+
+    var source: Operand
+}
+
+struct Operand: Equatable {
+    enum OpType: String, Codable, Equatable {
+        case local
+        case bool
+        case string_index
+    }
+
+    enum Value: Equatable {
+        case number(Int)
+        case bool(Bool)
+        case string_index(Int)
+    }
+
+    var type: OpType
+    var value: Value
+}
+
+// Apparently, when defining a custom initializer in the struct, it suppresses generation
+// of the default memberwise initializer, whereas when it is defined in an extension, we
+// get both.
+// ref: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/initialization/#Memberwise-Initializers-for-Structure-Types
+extension Operand: Codable {
+    enum CodingKeys: String, CodingKey {
+        case type
+        case value
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.type = try container.decode(OpType.self, forKey: .type)
+
+        switch self.type {
+        case .local:
+            let v = try container.decode(Int.self, forKey: .value)
+            self.value = Value.number(v)
+        case .bool:
+            let v = try container.decode(Bool.self, forKey: .value)
+            self.value = Value.bool(v)
+        case .string_index:
+            let v = try container.decode(Int.self, forKey: .value)
+            self.value = Value.string_index(v)
+        }
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        throw EncodingError.Unsupported
+    }
+
+}
--- Tests/SwiftRegoTests/SwiftRegoTests.swift
+++ Tests/SwiftRegoTests/SwiftRegoTests.swift
@@ -1,53 +1,184 @@
+import Foundation
 import Testing
 
 @testable import SwiftRego
 
+@preconcurrency
 struct decodeIRTestCase {
+    var name: String
     var json: String
     var policy: Policy
 }
 
 @Test(arguments: [
-    [
-        decodeIRTestCase(json: #"""
-                         {
-                           "static": {
-                             "strings": [
-                               {
-                                 "value": "result"
-                               },
-                               {
-                                 "value": "message"
-                               },
-                               {
-                                 "value": "world"
-                               }
-                             ],
-                             "files": [
-                               {
-                                 "value": "example.rego"
-                               }
-                             ]
-                           }
-                         }
-                         """#,
-                         policy: Policy(
-                            staticData: Static(
-                                strings: [
-                                   ConstString(value: "result"),
-                                   ConstString(value: "message"),
-                                   ConstString(value: "world")
-                                ],
-                                files: [
-                                    ConstString(value: "example.rego")
-                                ]
-                         )
-                     )
-                        ),
-    ]
-
-
-
+    decodeIRTestCase(
+        name: "static simple",
+        json: #"""
+            {
+              "static": {
+                "strings": [
+                  {
+                    "value": "result"
+                  },
+                  {
+                    "value": "message"
+                  },
+                  {
+                    "value": "world"
+                  }
+                ],
+                "files": [
+                  {
+                    "value": "example.rego"
+                  }
+                ]
+              }
+            }
+            """#,
+        policy: Policy(
+            staticData: Static(
+                strings: [
+                    ConstString(value: "result"),
+                    ConstString(value: "message"),
+                    ConstString(value: "world"),
+                ],
+                files: [
+                    ConstString(value: "example.rego")
+                ]
+            )
+        )
+    )
 ]) func decodeIR(tc: decodeIRTestCase) async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let actual = try JSONDecoder().decode(Policy.self, from: tc.json.data(using: .utf8)!)
+    #expect(actual == tc.policy)
+}
+
+@Test("decodeIR Statements")
+func decodeIRStatements() throws {
+    for tc in [
+        decodeIRTestCase(
+            name: "simple",
+            json: #"""
+                {"plans":{"plans":[{"name":"policy/hello","blocks":[{"stmts":[{"type":"CallStmt","stmt":{"func":"g0.data.policy.hello","args":[{"type":"local","value":0},{"type":"local","value":1}],"result":2,"file":9,"col":8,"row":0}},{"type":"AssignVarStmt","stmt":{"source":{"type":"local","value":2},"target":3,"file":0,"col":0,"row":0}},{"type":"MakeObjectStmt","stmt":{"target":4,"file":0,"col":0,"row":0}},{"type":"ObjectInsertStmt","stmt":{"key":{"type":"string_index","value":0},"value":{"type":"local","value":3},"object":4,"file":0,"col":0,"row":0}},{"type":"ResultSetAddStmt","stmt":{"value":4,"file":0,"col":0,"row":0}}]}]}]}}
+                """#,
+            policy: Policy()
+        )
+    ] {
+        let actual = try JSONDecoder().decode(Policy.self, from: tc.json.data(using: .utf8)!)
+        #expect(actual == tc.policy)
+    }
+}
+
+enum ResultExpectation<Success, Failure> {
+    case success(Success)
+    case failure(Failure)
+}
+
+@Test("decodeIR Operands")
+func decodeIROperands() throws {
+    struct testCase {
+        var name: String?
+        var json: String
+        var expected: ResultExpectation<Operand, Error.Type>
+    }
+
+    for tc in [
+        testCase(
+            name: "simple local",
+            json: #"""
+                {
+                    "type": "local",
+                    "value": 7
+                }
+                """#,
+            expected: .success(Operand(type: .local, value: .number(7)))
+        ),
+        testCase(
+            name: "simple bool",
+            json: #"""
+                {
+                    "type": "bool",
+                    "value": true
+                }
+                """#,
+            expected: .success(Operand(type: .bool, value: .bool(true)))
+        ),
+        testCase(
+            name: "simple string_index",
+            json: #"""
+                {
+                    "type": "string_index",
+                    "value": 123
+                }
+                """#,
+            expected: .success(Operand(type: .string_index, value: .string_index(123)))
+        ),
+        testCase(
+            name: "invalid type",
+            json: #"""
+                {
+                    "type": "invalid!",
+                    "value": 7
+                }
+                """#,
+            expected: .failure(DecodingError.self)
+        ),
+        testCase(
+            name: "missing type",
+            json: #"""
+                {
+                    "value": 7
+                }
+                """#,
+            expected: .failure(DecodingError.self)
+        ),
+        testCase(
+            name: "missing value",
+            json: #"""
+                {
+                    "type": "local",
+                }
+                """#,
+            //expected: .success(Operand(type: .local, value: .number(7)))  //.failure(DecodingError.self)
+            expected: .failure(DecodingError.self)
+        )
+    ] {
+        let result: Result<Operand, Error> = Result{
+             try JSONDecoder()
+                .decode(Operand.self, from: tc.json.data(using: .utf8)!)
+        }
+        switch tc.expected {
+        case .success(let expectedValue):
+            #expect(throws: Never.self, commentFor(tc.name)) {
+                // Check for unexpected exceptions separately (otherwise we can't customize the comment)
+                try result.get()
+            }
+            // TODO! Check with the Xcode folks - we don't seem to get the fancy comparison stuff
+            // for failures when we do #expect(try result.get() == expectedValue)
+            guard let actual = try? result.get() else {
+                return
+            }
+            #expect(actual == expectedValue, commentFor(tc.name))
+        case .failure(_):
+            // TODO check the exact error!!
+            #expect(throws: (any Error).self, commentFor(tc.name)) { try result.get() }
+//            #expect{try actual.get()} throws: { error in
+//                error is errType
+//            }
+//            do {
+//                try actual.get()
+//            }
+//            catch let err {
+//                #expect(err is errType.self)
+//            }
+//            #expect(error: errType) { try actual.get() }
+        }
+    }
+}
+
+func commentFor(_ name: String?) -> Comment {
+    guard let name else {
+        return Comment("[failed testcase: unnamed]")
+    }
+    return Comment(rawValue: "[failed testcase: \(name)]")
 }

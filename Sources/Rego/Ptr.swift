f93ef7841f8cffd9ebbf93bdcc6976a970e46272
Oren Shomron <shomron@apple.com>
2025-02-16 05:50:30 -0800

    perf: make Ptr final (#37)
    According to advice in:
    https://github.com/swiftlang/swift/blob/main/docs/OptimizationTips.rst#advice-use-final-when-you-know-the-declaration-does-not-need-to-be-overridden
    
    Benchmark shows small but measurable improvement.
    
    Signed-off-by: Oren Shomron <shomron@apple.com>

--- Sources/Rego/Ptr.swift
+++ Sources/Rego/Ptr.swift
@@ -2,7 +2,7 @@
 // It takes ownership of the provided value,
 // and then copies of the Ptr will reference the
 // same owned copy.
-package class Ptr<T> {
+package final class Ptr<T> {
     public var v: T
 
     init(toCopyOf v: T) {

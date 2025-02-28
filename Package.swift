fe59588df3f99623af47e569b80ed4ca1b60895c
Oren Shomron <shomron@apple.com>
2025-02-28 07:41:14 -0800

    support iOS builds
    Add platform minimal constraints to support building on iOS
    
    Signed-off-by: Oren Shomron <shomron@apple.com>

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ import class Foundation.ProcessInfo
 let package = Package(
     name: "SwiftRego",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v13),
+        .iOS(.v16),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.

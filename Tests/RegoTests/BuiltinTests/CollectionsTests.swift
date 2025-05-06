69eef4464e996e6a18420fd61c7d5f4a8bddb39a
Dmitry Frenkel <d_frenkel@apple.com>
2025-05-06 13:21:31 -0700

    builtins: product, sort, sum
    Also: implementing internal.member_3 for compliance testing
    
    Resolves #12
    
    Signed-off-by: Dmitry Frenkel <d_frenkel@apple.com>

--- Tests/RegoTests/BuiltinTests/CollectionsTests.swift
+++ Tests/RegoTests/BuiltinTests/CollectionsTests.swift
@@ -86,9 +86,143 @@ extension BuiltinTests.CollectionsTests {
         ),
     ]
 
+    static let isMemberOfWithKeyTests: [BuiltinTests.TestCase] = [
+        BuiltinTests.TestCase(
+            description: "index, value in array -> true",
+            name: "internal.member_3",
+            args: [
+                1,
+                42,
+                ["c", 42, "d"],
+            ],
+            expected: .success(true)
+        ),
+        BuiltinTests.TestCase(
+            description: "index, wrong value in array -> false",
+            name: "internal.member_3",
+            args: [
+                1,
+                99,
+                ["c", 42, "d"],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "index out of bounds, value in array -> false",
+            name: "internal.member_3",
+            args: [
+                999,
+                42,
+                ["c", 42, "d"],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "index float with integer value, value in array -> false",
+            name: "internal.member_3",
+            args: [
+                1.0,
+                42,
+                ["c", 42, "d"],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "index not an integer, value in array -> false",
+            name: "internal.member_3",
+            args: [
+                1.5,
+                42,
+                ["c", 42, "d"],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "index not a number, value in array -> false",
+            name: "internal.member_3",
+            args: [
+                "index should be a number",
+                42,
+                ["c", 42, "d"],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "property, value in object -> true",
+            name: "internal.member_3",
+            args: [
+                "property",
+                42,
+                ["property": 42, "other": ["export": true]],
+            ],
+            expected: .success(true)
+        ),
+        BuiltinTests.TestCase(
+            description: "other, object value in object -> true",
+            name: "internal.member_3",
+            args: [
+                "other",
+                .object(["export": true]),
+                .object(["property": 42, "other": ["export": true]]),
+            ],
+            expected: .success(true)
+        ),
+        BuiltinTests.TestCase(
+            description: "property does not exist, value in object -> false",
+            name: "internal.member_3",
+            args: [
+                "property2",
+                42,
+                ["property": 42, "other": ["export": true]],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "property, different value in object -> false",
+            name: "internal.member_3",
+            args: [
+                "property",
+                0,
+                ["property": 42, "other": ["export": true]],
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "element, element in set -> true",
+            name: "internal.member_3",
+            args: [
+                10,
+                10,
+                .set([9, 10, 11]),
+            ],
+            expected: .success(true)
+        ),
+        BuiltinTests.TestCase(
+            description: "element, something else in set -> false",
+            name: "internal.member_3",
+            args: [
+                10,
+                9,
+                .set([9, 10, 11]),
+            ],
+            expected: .success(false)
+        ),
+        BuiltinTests.TestCase(
+            description: "element not in set -> false",
+            name: "internal.member_3",
+            args: [
+                100,
+                10,
+                .set([9, 10, 11]),
+            ],
+            expected: .success(false)
+        ),
+    ]
+
     static var allTests: [BuiltinTests.TestCase] {
         [
-            isMemberOfTests
+            isMemberOfTests,
+            isMemberOfWithKeyTests,
         ].flatMap { $0 }
     }
 

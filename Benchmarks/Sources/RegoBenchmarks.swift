28e2032da59bb9bd645c98d4430a64984b26f08a
Teemu Koponen <tkoponen@apple.com>
2025-12-18 11:55:26 -0800

    benchmarks: A trivial dynamic call benchmark.
    Signed-off-by: Teemu Koponen <tkoponen@apple.com>

--- Benchmarks/Sources/RegoBenchmarks.swift
+++ Benchmarks/Sources/RegoBenchmarks.swift
@@ -4,19 +4,64 @@ import Foundation
 internal import Rego
 
 let benchmarks: @Sendable () -> Void = {
+    let simpleBundleURL = URL(fileURLWithPath: "../Tests/RegoTests/TestData/Bundles/simple-directory-bundle")
+    let dynamicCallBundleURL = URL(fileURLWithPath: "../Tests/RegoTests/TestData/Bundles/dynamic-call-bundle")
+
     Benchmark(
         "Simple Policy Evaluation",
         configuration: .init(metrics: [.wallClock, .mallocCountTotal])
     ) { benchmark in
         // Setup OPA engine
         var engine = OPA.Engine(
-            bundlePaths: [OPA.Engine.BundlePath(name: "simple", url: URL(fileURLWithPath: "../Tests/RegoTests/TestData/Bundles/simple-directory-bundle"))])
+            bundlePaths: [OPA.Engine.BundlePath(name: "simple", url: simpleBundleURL)])
         let preparedQuery = try! await engine.prepareForEvaluation(query: "data.app.rbac.allow")
 
         let input: AST.RegoValue = [
             "user": "alice",
             "action": "read",
             "resource": "document123",
+        ]
+
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            let result = try! await preparedQuery.evaluate(input: input)
+            blackHole(result)
+        }
+    }
+
+    Benchmark(
+        "Dynamic Call - Double",
+        configuration: .init(metrics: [.wallClock, .mallocCountTotal])
+    ) { benchmark in
+        // Setup OPA engine
+        var engine = OPA.Engine(
+            bundlePaths: [OPA.Engine.BundlePath(name: "dynamic", url: dynamicCallBundleURL)])
+        let preparedQuery = try! await engine.prepareForEvaluation(query: "data.test")
+
+        let input: AST.RegoValue = [
+            "operation": "double",
+            "value": 42,
+        ]
+
+        benchmark.startMeasurement()
+        for _ in benchmark.scaledIterations {
+            let result = try! await preparedQuery.evaluate(input: input)
+            blackHole(result)
+        }
+    }
+
+    Benchmark(
+        "Dynamic Call - Square",
+        configuration: .init(metrics: [.wallClock, .mallocCountTotal])
+    ) { benchmark in
+        // Setup OPA engine
+        var engine = OPA.Engine(
+            bundlePaths: [OPA.Engine.BundlePath(name: "dynamic", url: dynamicCallBundleURL)])
+        let preparedQuery = try! await engine.prepareForEvaluation(query: "data.test")
+
+        let input: AST.RegoValue = [
+            "operation": "square",
+            "value": 42,
         ]
 
         benchmark.startMeasurement()

591b71c38e59b6427581c8d53616485e1875e1d8
Philip Conrad <philip@chariot-chaser.net>
2026-02-26 17:19:42 -0500

    deps: Bump swift-crypto to pick up newer APIs. (#108)
    Signed-off-by: Philip Conrad <philip@chariot-chaser.net>

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.

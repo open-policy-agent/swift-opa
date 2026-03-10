eca2b6e73ed911ff04ccfd21a575a0fd78bd8a45
Philip Conrad <philip_conrad@apple.com>
2026-03-10 14:03:35 -0400

    Rego/Bundle: Make Bundle and Manifest inits public.
    Signed-off-by: Philip Conrad <philip_conrad@apple.com>

--- Sources/Rego/Bundle.swift
+++ Sources/Rego/Bundle.swift
@@ -31,11 +31,21 @@ extension OPA {
             case regoV0 = 0
             case regoV1 = 1
         }
+
+        public init(
+            revision: String = "", roots: [String] = [""], regoVersion: Version = .regoV1,
+            metadata: AST.RegoValue = .null
+        ) {
+            self.revision = revision
+            self.roots = roots
+            self.regoVersion = regoVersion
+            self.metadata = metadata
+        }
     }
 }
 
 extension OPA.Bundle {
-    init(
+    public init(
         manifest: OPA.Manifest = OPA.Manifest(), planFiles: [BundleFile] = [], regoFiles: [BundleFile] = [],
         data: AST.RegoValue = .object([:])
     ) throws(BundleError) {
@@ -56,7 +66,7 @@ extension OPA.Bundle {
         self.rootsTrie = trie
     }
 
-    enum BundleError: Swift.Error {
+    public enum BundleError: Swift.Error {
         case overlappingRoots(String)
         case internalError(String)
     }

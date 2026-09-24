b6329459283cd47dbe81d9823a62dd149c4d8e99
Philip Conrad <philip_conrad@apple.com>
2026-09-24 18:28:20 -0400

    deps: Update swift-crypto version ranges for the 5.0.0 release.
    Signed-off-by: Philip Conrad <philip_conrad@apple.com>

--- Package.swift
+++ Package.swift
@@ -119,11 +119,11 @@ let package = Package(
 // that have a direct dependency on swift-crypto accept beta releases as well.
 if ProcessInfo.processInfo.environment["SWIFT_OPA_ALLOW_SWIFT_CRYPTO_BETA"] == nil {
     package.dependencies += [
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"6.0.0")
     ]
 } else {
     print("Accepting beta versions of swift-crypto!")
     package.dependencies += [
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0-beta.max")
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"6.0.0-beta.max")
     ]
 }
